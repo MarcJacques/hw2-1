395083de3aba32dd3e7ff79f3817862c76bf8c0f
Marc Jacques <39604945+MarcJacques@users.noreply.github.com>
2023-01-25 23:46:18 +0000

    add iomanip library add constant for Velocity factor calculate velocity of sound display to user

--- .ccls-cache/@home@runner@hw2-1/hw2-1.cpp
+++ .ccls-cache/@home@runner@hw2-1/hw2-1.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
+#include <iomanip>
+using namespace std;
 
-int main() {
-  std::cout << "Hello World!\n";
+int main() 
+{
+  double temp, velocity;
+  const double VELOCITY_FACTOR =  331.3;
+  cout << "Enter Celsius temperature: ";
+  cin >> temp;
+  //calculate velocity
+  velocity = VELOCITY_FACTOR + (0.61 * temp);
+  
+  cout << fixed << showpoint << setprecision(2);
+  cout <<"At "<<temp<<" degrees Celsius the velocity of sound is " <<velocity<< " m/s.";
+
+
+  
+  return 0; 
+  
 }
